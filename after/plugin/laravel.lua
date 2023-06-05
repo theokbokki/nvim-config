7cc690a0e9cde1b759f58ebd5d30cc4955a1cd85
&#10059 <90846584+theoleonet@users.noreply.github.com>
2023-06-05 11:39:26 +0200

    🔧 Update the laravel plugin config

--- after/plugin/laravel.lua
+++ after/plugin/laravel.lua
@@ -3,7 +3,12 @@ if not laravel_ok then
 	return
 end
 
-laravel.setup()
+laravel.setup {
+	route_info = {
+		enable = true,
+		position = "top",
+	},
+}
 
 local telescope_ok, telescope = pcall(require, "telescope")
 if not telescope_ok then
