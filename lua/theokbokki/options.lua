8fd4f390a64c5faed5843ed3566cde9a451b19d0
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-10-20 21:36:44 +0200

    Add numbers

--- lua/theokbokki/options.lua
+++ lua/theokbokki/options.lua
@@ -1,20 +1,20 @@
 local options = {
-    autoindent = true, -- copy indent from current line when starting new one
-    clipboard = 'unnamedplus', -- make vim clipboard the same as mac clipboard
-    colorcolumn="80,120", -- add 2 column to make my code cleaner
-    ignorecase = true, -- ignore case when searching
-    -- number = true, -- show absolute line number on cursor line (when relative number is on)
-    -- relativenumber = true, -- show relative line numbers
-    scrolloff = 999, -- keep current line in the center
-    shiftwidth = 4, -- use four spaces for indent width
-    smartcase = true, -- make search case sensitive if there are multiple cases
-    swapfile = false, -- remove swap files
-    tabstop = 4, -- make tab 4 spaces wide
-    termguicolors = true, -- make colors pretty
+	autoindent = true, -- copy indent from current line when starting new one
+	clipboard = "unnamedplus", -- make vim clipboard the same as mac clipboard
+	colorcolumn = "80,120", -- add 2 column to make my code cleaner
+	ignorecase = true, -- ignore case when searching
+	number = true, -- show absolute line number on cursor line (when relative number is on)
+	signcolumn = "number",
+	scrolloff = 999, -- keep current line in the center
+	shiftwidth = 4, -- use four spaces for indent width
+	smartcase = true, -- make search case sensitive if there are multiple cases
+	swapfile = false, -- remove swap files
+	tabstop = 4, -- make tab 4 spaces wide
+	termguicolors = true, -- make colors pretty
 }
 
 for k, v in pairs(options) do
-    vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 vim.g.mapleader = ","
