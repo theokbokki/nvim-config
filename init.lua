b594c1d4c01990ab236a644b7fc82f36c3af3cb4
théoo <90846584+theokbokki@users.noreply.github.com>
2025-10-27 10:16:09 +0100

    Change colorscheme

--- init.lua
+++ init.lua
@@ -34,71 +34,6 @@ map("n", "N", "Nzzzv")
 -- Don't override the current clipboard
 map("x", "<leader>p", [["_dP]])
 
--- Go to defintion
-map("n", "gd", vim.lsp.buf.definition)
-
--- LSP
-vim.lsp.config["phpactor"] = {
-    cmd = { 'phpactor', 'language-server' },
-    filetypes = { 'php' },
-    root_dir = function(bufnr, on_dir)
-        local fname = vim.api.nvim_buf_get_name(bufnr)
-        local cwd = assert(vim.uv.cwd())
-        local root = vim.fs.root(fname, { 'composer.json', '.git', '.phpactor.json', '.phpactor.yml' })
-
-        -- prefer cwd if root is a descendant
-        on_dir(root and vim.fs.relpath(cwd, root) and cwd)
-    end,
-}
-
-vim.lsp.config["ts_ls"] = {
-    init_options = { hostInfo = 'neovim' },
-    cmd = { 'typescript-language-server', '--stdio' },
-    filetypes = {'javascript'},
-    root_markers = {'package.json', '.git'},
-    handlers = {
-        -- handle rename request for certain code actions like extracting functions / types
-        ['_typescript.rename'] = function(_, result, ctx)
-            local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
-            vim.lsp.util.show_document({
-                uri = result.textDocument.uri,
-                range = {
-                    start = result.position,
-                    ['end'] = result.position,
-                },
-            }, client.offset_encoding)
-            vim.lsp.buf.rename()
-            return vim.NIL
-        end,
-    },
-    on_attach = function(client)
-        -- ts_ls provides `source.*` code actions that apply to the whole file. These only appear in
-        -- `vim.lsp.buf.code_action()` if specified in `context.only`.
-        vim.api.nvim_buf_create_user_command(0, 'LspTypescriptSourceAction', function()
-            local source_actions = vim.tbl_filter(function(action)
-                return vim.startswith(action, 'source.')
-            end, client.server_capabilities.codeActionProvider.codeActionKinds)
-
-            vim.lsp.buf.code_action({
-                context = {
-                    only = source_actions,
-                },
-            })
-        end, {})
-    end,
-}
-
-vim.lsp.enable({
-    "phpactor",
-    "ts_ls",
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(ev)
-        vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, { autotrigger = false })
-    end,
-})
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -120,17 +55,37 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         -- Colorscheme
-        {
-            "pbrisbin/vim-colors-off",
+        -- {
+        --     "pbrisbin/vim-colors-off",
+        --     config = function()
+        --         vim.cmd("colorscheme off")
+        --
+        --         vim.opt.background = "light";
+        --
+        --         vim.defer_fn(function()
+        --             vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#C6C6C6", bold = true })
+        --         end, 0)
+        --     end
+        -- },
+        { 
+            "catppuccin/nvim", 
+            name = "catppuccin",
+            priority = 1000,
             config = function()
-                vim.cmd("colorscheme off")
+                require("catppuccin").setup({
+                    flavour = "latte",
+                    transparent_background = true,
+                    float = {
+                        transparent = true,
+                        solid = false,
+                    },
+                })
 
-                vim.opt.background = "light";
-
-                vim.defer_fn(function()
-                    vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#C6C6C6", bold = true })
-                end, 0)
+                vim.cmd.colorscheme "catppuccin-latte"
             end
+        },
+        {
+            "xiyaowong/transparent.nvim",
         },
 
         -- Finder
@@ -182,23 +137,18 @@ require("lazy").setup({
             end,
         },
 
-        -- LSP
-        {
-            "mason-org/mason.nvim",
-            config = function()
-                require("mason").setup()
-            end,
-        },
-
         -- Treesitter
         {
             'nvim-treesitter/nvim-treesitter',
             config = function()
                 require("nvim-treesitter.configs").setup({
-                    ensure_installed = {"php"},
+                    ensure_installed = {"php", "lua", "javascript", "html", "css", "scss"},
                     indent = {
                         enable = true,
                     },
+                    highlight = {
+                        enable = true,
+                    }
                 })
             end,
         }
